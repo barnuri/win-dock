355dbd1c76ba9c3082d45784a904051fb8d61915
Bar Nuri <barnuri@hotmail.com>
2025-09-03 14:39:32 +0300

    Bump version to 1.0.31

--- homebrew-tap/Formula/windock.rb
+++ homebrew-tap/Formula/windock.rb
@@ -1,8 +1,8 @@
 class Windock < Formula
   desc "Windows 11-style taskbar for macOS"
   homepage "https://github.com/barnuri/win-dock"
-  url "https://github.com/barnuri/win-dock/releases/download/v1.0.30/WinDock.zip"
-  version "1.0.30"
+  url "https://github.com/barnuri/win-dock/releases/download/v1.0.31/WinDock.zip"
+  version "1.0.31"
   sha256 :no_check
 
   depends_on macos: :sonoma
